9a15e70817fe9da04dc2ec4546ccfcf987219a63
MirandaGomezMn <mgomez@seidoranalytics.com>
2025-01-31 16:59:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.write("The name on your Smoothie will be: ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 # Convert the snowpark dataframe to a pandas dataframe so we can use de LOC function
@@ -53,5 +53,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon='✅')
         
-
 
